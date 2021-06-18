6c2553c836ad60e8a77bd30eb8380fa5ec877578
cghosh828049 <cghosh828049@gmail.com>
2021-06-18 19:53:40 +0530

    Added a sidebar and leaderboard section

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ var rows = 4, cols = 4;
 var gridArr = new Array(rows*cols);
 var moves = 0;
 var time = 0;
-var isPaused = false;
+var isPaused = true;
 var gameOver = false;
+var aboutMeShown = false;
+var howToShown = false;
 var blank;
+var bestGameMove = new Array(5);
+var bestGameTime = new Array(5);
+var bestGameCount;
 
 function showTime() {
     if(!isPaused) {
@@ -37,6 +42,8 @@ function createGame() {
         }
         gamespace.appendChild(rowDiv);
     }
+    getBest();
+    displayBest();
     
 }
 
@@ -60,7 +67,10 @@ function start() {
     }
     moves = 0;
     time = 0;
-    isPaused = false;
+    if(isPaused) {
+        isPaused = false;
+        document.getElementById("pause").innerHTML = "Pause";
+    }
     timeInterval;
     console.log(gridArr);
 }
@@ -99,6 +109,13 @@ function isOver() {
         document.getElementById("movewin").innerHTML = moves;
         document.getElementsByClassName("win-div")[0].style.visibility = "visible";
         document.getElementsByClassName("win-div")[0].style.opacity = "1";
+        if(bestGameCount <10) {
+            bestGameMove[bestGameCount] = moves;
+            bestGameTime[bestGameCount] = time;
+            bestGameCount++;
+            sortBestGame();
+            storeBest();
+        }
     }
 }
 
@@ -110,6 +127,8 @@ function newGame() {
         gameOver = false;
         isPaused = false;
     } 
+    getBest();
+    displayBest();
     start();
     // uncomment the below line to see the winning section.
     //gridArr = new Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,0,15);
@@ -169,5 +188,103 @@ function pause() {
             isPaused = true;
             document.getElementById("pause").innerHTML = "Resume";
         }
+    }
+}
+
+function newMode() {
+    document.getElementsByClassName("selection-div")[0].style.opacity = "1";
+    document.getElementsByClassName("selection-div")[0].style.visibility = "visible";
+    var gamespace = document.getElementsByClassName("game-grid-div")[0];
+    for(var i = 0 ; i < rows ; i++) {
+        gamespace.removeChild(gamespace.firstElementChild);
+    }
+    gameOver = true;
+    isPaused = true;
+    moves = 0;
+    time = 0;
+    document.getElementById("move").innerHTML = moves;
+    document.getElementById("time").innerHTML = time +"s";
+}
+
+function aboutMe() {
+    if(aboutMeShown) {
+        aboutMeShown = false;
+        document.getElementById("aboutMe").style.opacity = "0";
+        document.getElementById("aboutMe").style.visibility = "hidden";
+    }
+    else {
+        aboutMeShown = true;
+        document.getElementById("aboutMe").style.opacity = "1";
+        document.getElementById("aboutMe").style.visibility = "visible";
+    }
+}
+
+function howToPlay() {
+    if(howToShown) {
+        howToShown = false;
+        document.getElementById("howToPlay").style.opacity = "0";
+        document.getElementById("howToPlay").style.visibility = "hidden";
+    }
+    else {
+        howToShown = true;
+        document.getElementById("howToPlay").style.opacity = "1";
+        document.getElementById("howToPlay").style.visibility = "visible";
+    }
+}
+
+function storeBest() {
+    localStorage.clear();
+    localStorage.setItem("count", bestGameCount);
+    for(var i = 0; i < bestGameCount; i++) {
+        localStorage.setItem("move"+i, bestGameMove[i]);
+        localStorage.setItem("time"+i, bestGameTime[i]);
+    }
+}
+
+function getBest() {
+    bestGameCount = localStorage.getItem("count");
+    if(bestGameCount == null) bestGameCount = 0;
+    for(var i = 0 ; i < bestGameCount ; i++) {
+        bestGameMove[i] = localStorage.getItem("move"+i);
+        bestGameTime[i] = localStorage.getItem("time"+i);
+    }
+}
+
+function sortBestGame(){
+     
+    for(var i = 0; i < bestGameCount-1; i++){ 
+      for(var j = 0; j < ( bestGameCount - i -1 ); j++){
+
+        if(bestGameMove[j] > bestGameMove[j+1]){
+            
+          var temp = bestGameMove[j];
+          bestGameMove[j] = bestGameMove[j + 1];
+          bestGameMove[j+1] = temp;
+
+          temp = bestGameTime[j];
+          bestGameTime[j] = bestGameTime[j + 1];
+          bestGameTime[j+1] = temp;
+        }
+      }
+    }
+
+    if(bestGameCount == 10) {
+        bestGameCount--;
+    }
+}
+
+function displayBest() {
+    var leaderboard = document.getElementById("leaderboard");
+    while(leaderboard.firstChild) {
+        leaderboard.removeChild(leaderboard.firstChild);
+    }
+    for(var i = 0 ; i < bestGameCount && i < 5 ; i++) {
+        var label = document.createElement("div");
+        label.classList.add("label-div", "label-div--best");
+        var status = document.createElement("span");
+        status.className = "status-label";
+        status.innerHTML = (i+1)+". Moves : "+bestGameMove[i]+" Time : "+bestGameTime[i]+"s";
+        label.appendChild(status);
+        leaderboard.appendChild(label);
     }
 }
